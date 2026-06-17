dbfb6199e73b4af1fb37749bc43e4291fef75bdd
bsmjsambalpuri <83930752+bsmjsambalpuri@users.noreply.github.com>
2026-06-17 19:45:55 +0530

    adding 5ony

--- process_playlist.py
+++ process_playlist.py
@@ -7,7 +7,7 @@ PLAYLIST_URLS = [
     "https://raw.githubusercontent.com/amazeyourself/m3u/refs/heads/main/ekamraott.m3u",
     "https://raw.githubusercontent.com/amazeyourself/m3u/refs/heads/main/jtv.m3u",
     "https://raw.githubusercontent.com/amazeyourself/m3u/refs/heads/main/samsungtvplus/all.m3u",
-     "https://raw.githubusercontent.com/hasanhabibmottakin/xxxxxxxxxxxxxxxxxx/refs/heads/main/ott.m3u"
+     "https://raw.githubusercontent.com/hasanhabibmottakin/Sony-Liv-Channels/refs/heads/main/playlist.m3u"
 ]
 
 def load_whitelist():
